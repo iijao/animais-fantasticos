8a6a5ed80534529d6a6bd3fc6d1a1ca9e2a49ed0
iijao <joaovmag10@gmail.com>
2026-01-20 14:50:35 -0300

    tab nav tranformado em class

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,21 +1,27 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
+export default class TabNav {
+  constructor(menu, content){
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
+  }
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
       section.classList.remove('ativo');
     });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add('ativo', direcao);
   }
-
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  //add evento nas tab
+  addTabNavEvent(){
+        this.tabMenu.forEach((itemMenu, index) => {
+          itemMenu.addEventListener('click', () => this.activeTab(index));
     });
+  }
+  init(){
+    if (this.tabMenu.length && this.tabContent.length){
+      //ativar primeiro item
+      this.activeTab(0)
+      this.addTabNavEvent();
+    }
   }
 }
