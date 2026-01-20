fad3a41e12ad8062d097199f6b7b8d3583a7de36
iijao <joaovmag10@gmail.com>
2026-01-20 19:48:19 -0300

    2adicionado bind ao accordion

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -2,25 +2,28 @@ export default class Accordion {
   constructor(list) {
     this.accordionList = document.querySelectorAll(list);
     this.activeClass = 'ativo';
+    
+    // ESTA É A CHAVE: Garante que o 'this' em toggleAccordion 
+    // seja SEMPRE a classe Accordion
     this.toggleAccordion = this.toggleAccordion.bind(this);
   }
 
   toggleAccordion(item) {
-    // Usamos o 'item' recebido para dar o toggle, não o 'this'
+    // Agora 'this.activeClass' vai funcionar sempre
     item.classList.toggle(this.activeClass);
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
-      // Usamos uma arrow function para passar o item sem executar a função na hora
+      // Agora podemos passar a função diretamente
       item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
 
   init() {
     if (this.accordionList.length) {
-      // Ativar primeiro item
+      // Agora o 'this' não será undefined aqui
       this.toggleAccordion(this.accordionList[0]);
       this.addAccordionEvent();
     }
