1c6cea94a0d2a15abad98e5efc1b58f003bba512
iijao <joaovmag10@gmail.com>
2026-01-26 19:58:36 -0300

    refatoraçao com class do scroll anima

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
+import ScrollAnima from './modules/scroll-anima.js';
 
 
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -30,9 +30,12 @@ tooltip.init();
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
+scrollAnima.init()
+
+
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 
-
-initAnimacaoScroll();
